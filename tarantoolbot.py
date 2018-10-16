576de11c5ca93a1ad323459d5cb667cdb3c65a08
Konstantin Nazarov <mail@racktear.com>
2018-10-16 14:35:40 +0300

    Run autopep8 to make code conform to PEP8

--- tarantoolbot.py
+++ tarantoolbot.py
@@ -22,17 +22,19 @@ last_events = []
 # @param author GitHub login of the author.
 # @param action What to log.
 # @param body Message body.
+
+
 def create_event(author, action, body):
-	time = datetime.datetime.now().strftime('%b %d %H:%M:%S')
-	result = '<tr>'
-	result += '<td>{}</td>'.format(time)
-	result += '<td>{}</td>'.format(author)
-	result += '<td>{}</td>'.format(action)
-	result += '<td>{}</td>'.format(body)
-	result += '</tr>'
-	last_events.append(result)
-	if len(last_events) > LAST_EVENTS_SIZE:
-		last_events.pop(0)
+    time = datetime.datetime.now().strftime('%b %d %H:%M:%S')
+    result = '<tr>'
+    result += '<td>{}</td>'.format(time)
+    result += '<td>{}</td>'.format(author)
+    result += '<td>{}</td>'.format(action)
+    result += '<td>{}</td>'.format(body)
+    result += '</tr>'
+    last_events.append(result)
+    if len(last_events) > LAST_EVENTS_SIZE:
+        last_events.pop(0)
 
 ##
 # Bot to track documentation update requests
@@ -53,42 +55,48 @@ def create_event(author, action, body):
 # @retval not None - @A body is a well-formatted
 #         request. Returned value is a dictionary
 #         with 'title' and 'description'.
+
+
 def parse_comment(body):
-	if not body.startswith(bot_name):
-		return None, None
-	offset = len(bot_name)
-	for dr in doc_requests:
-		if body.startswith(dr, offset):
-			offset += len(dr)
-			break
-	else:
-		return None, 'Invalid request type.'
-	if not body.startswith(title_header, offset):
-		return None, 'Title not found.'
-	offset += len(title_header)
-	pos = body.find('\n', offset)
-	if pos == -1:
-		pos = len(body)
-	return {'title': body[offset:pos],
-		'description': body[pos:]}, None
+    if not body.startswith(bot_name):
+        return None, None
+    offset = len(bot_name)
+    for dr in doc_requests:
+        if body.startswith(dr, offset):
+            offset += len(dr)
+            break
+    else:
+        return None, 'Invalid request type.'
+    if not body.startswith(title_header, offset):
+        return None, 'Title not found.'
+    offset += len(title_header)
+    pos = body.find('\n', offset)
+    if pos == -1:
+        pos = len(body)
+    return {'title': body[offset:pos],
+            'description': body[pos:]}, None
 ##
 # Send a comment to an issue. Sent comment is either
 # parser error message, or notification about accepting
 # a request.
+
+
 def send_comment(body, issue_api, to):
-	create_event(to, 'send_comment', body)
-	body = {'body': '@{}: {}'.format(to, body)}
-	url = '{}/comments?access_token={}'.format(issue_api, token)
-	r = requests.post(url, json=body)
-	print('Sent comment: {}'.format(r.status_code))
+    create_event(to, 'send_comment', body)
+    body = {'body': '@{}: {}'.format(to, body)}
+    url = '{}/comments?access_token={}'.format(issue_api, token)
+    r = requests.post(url, json=body)
+    print('Sent comment: {}'.format(r.status_code))
 
 ##
 # Get all the comments of an issue.
+
+
 def get_comments(issue_api):
-	body = {'since': '1970-01-01T00:00:00Z'}
-	url = '{}/comments?access_token={}'.format(issue_api, token)
-	r = requests.get(url, json=body)
-	return r.json()
+    body = {'since': '1970-01-01T00:00:00Z'}
+    url = '{}/comments?access_token={}'.format(issue_api, token)
+    r = requests.get(url, json=body)
+    return r.json()
 
 ##
 # Open a new issue in a documentation repository.
@@ -97,14 +105,16 @@ def get_comments(issue_api):
 # @param src_url Link to a webpage with the original issue or
 #        commit.
 # @param author Github login of the request author.
+
+
 def create_issue(title, description, src_url, author):
-	create_event(author, 'create_issue', title)
-	description = '{}\nRequested by @{} in {}.'.format(description, author,
-							   src_url)
-	body = {'title': title, 'body': description}
-	url = '{}/issues?access_token={}'.format(doc_repo_url, token)
-	r = requests.post(url, json=body)
-	print('Created issue: {}'.format(r.status_code))
+    create_event(author, 'create_issue', title)
+    description = '{}\nRequested by @{} in {}.'.format(description, author,
+                                                       src_url)
+    body = {'title': title, 'body': description}
+    url = '{}/issues?access_token={}'.format(doc_repo_url, token)
+    r = requests.post(url, json=body)
+    print('Created issue: {}'.format(r.status_code))
 
 ##
 # Check that a new or edited comment for an issue is the request.
@@ -116,26 +126,28 @@ def create_issue(title, description, src_url, author):
 #        response is wrote.
 #
 # @retval Response to a GitHub hook.
+
+
 def process_issue_comment(body, issue_state, issue_api):
-	action = body['action']
-	if (action != 'created' and action != 'edited') or \
-	   issue_state != 'open':
-		return 'Not needed.'
-	comment = body['comment']
-	author = comment['user']['login']
-	comment, error = parse_comment(comment['body'])
-	if error:
-		print('Error during request processing: {}'.format(error))
-		send_comment(error, issue_api, author)
-	elif comment:
-		print('Request is processed ok')
-		if action == 'edited':
-			send_comment('Accept edited.', issue_api, author)
-		else:
-			send_comment('Accept.', issue_api, author)
-	else:
-		print('Ignore non-request comments')
-	return 'Doc request is processed.'
+    action = body['action']
+    if (action != 'created' and action != 'edited') or \
+       issue_state != 'open':
+        return 'Not needed.'
+    comment = body['comment']
+    author = comment['user']['login']
+    comment, error = parse_comment(comment['body'])
+    if error:
+        print('Error during request processing: {}'.format(error))
+        send_comment(error, issue_api, author)
+    elif comment:
+        print('Request is processed ok')
+        if action == 'edited':
+            send_comment('Accept edited.', issue_api, author)
+        else:
+            send_comment('Accept.', issue_api, author)
+    else:
+        print('Ignore non-request comments')
+    return 'Doc request is processed.'
 
 ##
 # Check that a just closed issue contains contains
@@ -148,18 +160,20 @@ def process_issue_comment(body, issue_state, issue_api):
 # @param issue_url Public URL of the original issue.
 #
 # @retval Response to a GitHub hook.
+
+
 def process_issue_state_change(body, issue_state, issue_api, issue_url):
-	action = body['action']
-	if action != 'closed':
-		return 'Not needed.'
-	comments = get_comments(issue_api)
-	comment = None
-	for c in comments:
-		comment, error = parse_comment(c['body'])
-		if comment:
-			create_issue(comment["title"], comment["description"],
-				     issue_url, c['user']['login'])
-	return 'Issue is processed.'
+    action = body['action']
+    if action != 'closed':
+        return 'Not needed.'
+    comments = get_comments(issue_api)
+    comment = None
+    for c in comments:
+        comment, error = parse_comment(c['body'])
+        if comment:
+            create_issue(comment["title"], comment["description"],
+                         issue_url, c['user']['login'])
+    return 'Issue is processed.'
 
 ##
 # Process a commit event, triggered on any push. If in the commit
@@ -168,56 +182,61 @@ def process_issue_state_change(body, issue_state, issue_api, issue_url):
 # @param c Commit object.
 # @param is_master_push True if the commit is pushed into the
 #        master branch.
+
+
 def process_commit(c, is_master_push):
-	body = c['message']
-	request_pos = body.find(bot_name)
-	if request_pos == -1:
-		return
-	request = body[request_pos:]
-	author = c['author']['username']
-	comment, error = parse_comment(request)
-	if error:
-		print('Error during request processing: {}'.format(error))
-		create_event(author, 'process_commit', error)
-	else:
-		create_event(author, 'process_commit', 'Accept')
-		if is_master_push:
-			create_issue(comment['title'], comment['description'],
-				     c['url'], author)
+    body = c['message']
+    request_pos = body.find(bot_name)
+    if request_pos == -1:
+        return
+    request = body[request_pos:]
+    author = c['author']['username']
+    comment, error = parse_comment(request)
+    if error:
+        print('Error during request processing: {}'.format(error))
+        create_event(author, 'process_commit', error)
+    else:
+        create_event(author, 'process_commit', 'Accept')
+        if is_master_push:
+            create_issue(comment['title'], comment['description'],
+                         c['url'], author)
+
 
 @post('/')
 def index_post():
-	r = request.json
-	t = request.get_header('X-GitHub-Event')
-	if 'issue' in r:
-		issue = r['issue']
-		if not 'state' in issue:
-			return 'Event is not needed.'
-		issue_state = issue['state']
-		issue_api = issue['url']
-		issue_url = issue['html_url']
+    r = request.json
+    t = request.get_header('X-GitHub-Event')
+    if 'issue' in r:
+        issue = r['issue']
+        if not 'state' in issue:
+            return 'Event is not needed.'
+        issue_state = issue['state']
+        issue_api = issue['url']
+        issue_url = issue['html_url']
 
-		if t == 'issue_comment':
-			return process_issue_comment(r, issue_state, issue_api)
-		elif t == 'issues':
-			return process_issue_state_change(r, issue_state,
-							  issue_api, issue_url)
-		else:
-			return 'Event "{}" is not needed.'.format(t)
-	elif t == 'push':
-		repo = r['repository']
-		branch = r['ref'].split('/')[-1]
-		is_master_push = repo['master_branch'] == branch
-		for c in r['commits']:
-			process_commit(c, is_master_push)
-	else:
-		return 'Event is not needed.'
+        if t == 'issue_comment':
+            return process_issue_comment(r, issue_state, issue_api)
+        elif t == 'issues':
+            return process_issue_state_change(r, issue_state,
+                                              issue_api, issue_url)
+        else:
+            return 'Event "{}" is not needed.'.format(t)
+    elif t == 'push':
+        repo = r['repository']
+        branch = r['ref'].split('/')[-1]
+        is_master_push = repo['master_branch'] == branch
+        for c in r['commits']:
+            process_commit(c, is_master_push)
+    else:
+        return 'Event is not needed.'
+
 
 @get('/')
 def index_get():
-	return ('<h1>{}</h1><table border="1" cellspacing="2" ' +\
-		'cellpadding="2">{}</table>').format('TarantoolBot Journal',
-						     ' '.join(last_events))
+    return ('<h1>{}</h1><table border="1" cellspacing="2" ' +
+            'cellpadding="2">{}</table>').format('TarantoolBot Journal',
+                                                 ' '.join(last_events))
+
 
 print('Starting bot')
 run(host='0.0.0.0', port=8888)
