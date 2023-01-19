tarantool/docbot

commit f1df824a79221609c0ea28cb126c5a1a2ec97b8d

Refactor docbot service
To make the project more flexible and extendable, tarantoolbot.py file
was split on the several files. Bottle framework was replaced with
Flask as it is easier to connect to Elastic APM server.

- `app.py` contains Flask config, WSGI application and endpoints.
- `github.py` has GitHub class to interact with its API.
- `handles.py` contains handlers which parse request data and
  transfers data to processors
- `processors.py` keeps GitHum commit and issue messages processors
- `settigns.py` - project config
- `utils.py` - project helpers

Added possibility to add any repository to the bot through
doc_repo_urls mapping in `settings.py`.

Added `deploy` workflow to push the project to Dokku server. The
workflow pushes code with `--force` flag, so it will override project
in Dokku even if Dokku repo has different branch or commits order.

`Dockerfile` was refactored for using lightweight python-3.10.4 image
based on Alpine.

To add new repository to the bot, do the following steps:
- add @TarantoolBot user to the source project and to the corresponding
  doc project as well.
- add source to docs repos mapping to `settings.py` in doc_repo_urls
  dictionary.
- push the changes to master, the project will be deployed.
- set up webhook to the source repository and point it to docbot
  service.

Closes #13
Closes #16
Part of tarantool/infra#77

docbot/settings.py
@@ -1,0 +1,16 @@
+import os
+
+token = os.environ.get('GITHUB_TOKEN')
+assert token is not None
+doc_requests = [' document\r\n', ' document\n']
+bot_name = '@TarantoolBot'
+title_header = 'Title:'
+api = 'https://api.github.com/repos/tarantool/'
+doc_repo_urls = {
+    f'tarantool/tarantool': f'{api}doc',
+    f'tarantool/tarantool-ee': f'{api}enterprise_doc',
+    f'tarantool/sdk': f'{api}enterprise_doc',
+    f'tarantool/tdg': f'{api}tdg-doc',
+    f'tarantool/tdg2': f'{api}tdg-doc',
+}
+LAST_EVENTS_SIZE = 30

.github/workflows/deploy.yml
@@ -1,0 +1,21 @@
+name: Deploy to Dokku
+
+on:
+  push:
+    branches:
+      - master
+jobs:
+  deploy:
+    runs-on: ubuntu-20.04-self-hosted
+    steps:
+      - uses: actions/checkout@v2
+        with:
+            fetch-depth: 0
+      - id: deploy
+        name: Deploy to dokku
+        uses: idoberko2/dokku-deploy-github-action@v1
+        with:
+            ssh-private-key: ${{ secrets.SSH_PRIVATE_KEY }}
+            dokku-host: ${{ secrets.DOCBOT_HOST }}
+            app-name: ${{ secrets.DOCBOT_APP }}
+            git-push-flags: '--force'

.gitignore
@@ -1,0 +1,5 @@
+venv
+.idea
+__pycache__
+*.pyc
+*.pyi

.travis.yml
@@ -1,28 +1,0 @@
-sudo: required
-services:
-  - docker
-
-branches:
-  only:
-    - master
-
-env: CONTAINER_NAME=tarantool/docbot:latest
-
-script:
-  - docker build -t ${CONTAINER_NAME} .
-
-after_success:
-  - docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
-  - docker push ${CONTAINER_NAME}
-
-  - "( apt-get update -y && apt-get install openssh-client -y ) || ( which ssh-agent
-    || yum install openssh -y )"
-
-  - openssl aes-256-cbc -K $encrypted_2272aa30aa52_key -iv $encrypted_2272aa30aa52_iv
-                                                  -in travis_key.enc -out travis_key -d
-
-  - eval $(ssh-agent -s)
-  - chmod 600 travis_key
-  - ssh-add travis_key
-
-  - ssh -o "StrictHostKeyChecking no" docbot-deployer@try.tarantool.org

Dockerfile
@@ -1,17 +1,13 @@
-FROM python:2
+FROM python:3.10.4-alpine
 
-RUN mkdir -p /usr/src/bot
-WORKDIR /usr/src/bot
+COPY requirements.txt /tmp
+RUN pip install --upgrade pip --no-cache-dir && \
+    pip install -r tmp/requirements.txt --no-cache-dir
 
-RUN pip install --upgrade pip
-
-COPY requirements.txt /usr/src/bot
-COPY tarantoolbot.py /usr/src/bot
-COPY write_credentials.sh /usr/src/bot
-
-RUN pip install -r requirements.txt
+COPY docbot /app/docbot
 
 ENV PORT=5000
 EXPOSE 5000
 
-CMD gunicorn --bind 0.0.0.0:$PORT tarantoolbot:app
+WORKDIR /app
+CMD gunicorn --bind 0.0.0.0:$PORT docbot.app:app

Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT tarantoolbot:app
+web: gunicorn --bind 0.0.0.0:$PORT docbot.app:app

docbot/app.py
@@ -1,0 +1,23 @@
+from elasticapm.contrib.flask import ElasticAPM
+from flask import Flask, request
+
+from .handlers import webhook_handler, list_events_handler
+
+app = Flask(__name__)
+
+app.config['ELASTIC_APM'] = {
+    'SERVICE_NAME': 'docbot',
+}
+apm = ElasticAPM(app)
+
+
+@app.route("/", methods=['GET'])
+def index() -> str:
+    return list_events_handler()
+
+
+@app.route("/", methods=['POST'])
+def webhook() -> str:
+    data: dict = request.json
+    event: str = request.headers.get('X-GitHub-Event')
+    return webhook_handler(data, event)

docbot/github.py
@@ -1,0 +1,39 @@
+import requests
+
+from .utils import create_event
+
+
+class GitHub:
+    def __init__(self, token: str) -> None:
+        self.token: str = token
+
+    def send_comment(self, body, issue_api, to):
+        create_event(to, 'send_comment', body)
+        body = {'body': '@{}: {}'.format(to, body)}
+        url = '{}/comments'.format(issue_api)
+        status_code = self._send_request(url, body)
+        print('Sent comment: {}'.format(status_code))
+
+    def get_comments(self, issue_api) -> dict:
+        body = {'since': '1970-01-01T00:00:00Z'}
+        url = '{}/comments'.format(issue_api)
+        r = self._send_request(url, body, method='get')
+        return r.json()
+
+    def create_issue(self, title, description, src_url, author, doc_repo_url):
+        create_event(author, 'create_issue', title)
+        description = '{}\nRequested by @{} in {}.'.format(description, author,
+                                                           src_url)
+        body = {'title': title, 'body': description}
+        url = '{}/issues'.format(doc_repo_url)
+        status_code = self._send_request(url, body)
+        print('Created issue: {}'.format(status_code))
+
+    def _send_request(self, url, body, method='post'):
+        headers = {
+            'Authorization': 'token {}'.format(self.token),
+        }
+        r = requests.request(method, url, json=body, headers=headers)
+        print(r.status_code)
+        r.raise_for_status()
+        return r

commit c4430ae2a9d65327d2f2e425f2a951644df8d670

Add vshard into tracked repos back
The problem is mentioned in #14, but I'll leave the issue open, because
the original problem is still here.

It seems the regression was introduced in f1df824a7922 ("Refactor docbot
service").

docbot/settings.py
@@ -8,6 +8,7 @@ title_header = 'Title:'
 api = 'https://api.github.com/repos/tarantool/'
 doc_repo_urls = {
     'tarantool/tarantool': f'{api}doc',
+    'tarantool/vshard': f'{api}doc',
     'tarantool/tarantool-ee': f'{api}enterprise_doc',
     'tarantool/sdk': f'{api}enterprise_doc',
     'tarantool/tdg': f'{api}tdg-doc',